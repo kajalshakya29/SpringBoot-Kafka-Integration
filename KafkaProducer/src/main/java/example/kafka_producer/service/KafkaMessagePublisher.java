f83717be6dd00f287255caff7ba981513ceef9db
kajal shakya <157579940+kajalshakya29@users.noreply.github.com>
2026-02-03 16:56:45 +0530

    Add files via upload

---
+++ KafkaProducer/src/main/java/example/kafka_producer/service/KafkaMessagePublisher.java
@@ -0,0 +1,25 @@
+package example.kafka_producer.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class KafkaMessagePublisher {
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void sendMessageToTopic(String message) {
+        CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send("kajal-topic1", message);
+        future.whenComplete((result,ex)-> {
+            if (ex==null){
+                System.out.println("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }else {
+                System.out.println("unable to send message=[" + message + "] due to: " +ex.getMessage());
+            }
+        });
+    }
+}
